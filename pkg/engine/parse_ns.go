27891f681432ee4b593b615b962984c19f18d53e
mayank <mayank.patel@mayadata.io>
2019-12-16 12:48:40 +0530

    Updating logging for moveengine
    Signed-off-by: mayank <mayank.patel@mayadata.io>

--- pkg/engine/parse_ns.go
+++ pkg/engine/parse_ns.go
@@ -1,8 +1,7 @@
 package engine
 
 import (
-	"fmt"
-
+	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -12,16 +11,14 @@ func (m *MoveEngineAction) parseNamespace(api metav1.APIResource) error {
 		len(m.mov.Spec.RemoteNamespace) != 0 {
 		obj, err := m.getResourceFromAPI(api, client.ObjectKey{Name: m.mov.Spec.Namespace})
 		if err != nil {
-			fmt.Printf("Failed to fetch resource %v\n", err)
-			return err
+			return errors.Wrapf(err, "Failed to fetch namespace %v", m.mov.Spec.Namespace)
 		}
 		obj.SetName(m.mov.Spec.RemoteNamespace)
 		m.addToResourceList(obj)
 	} else {
 		list, err := m.ListResourcesFromAPI(api)
 		if err != nil {
-			fmt.Printf("Failed to fetch list for %v %v.. %v\n", api.Name, api.Group, err)
-			return err
+			return errors.Wrapf(err, "Failed to fetch namespace list")
 		}
 		for _, i := range list.Items {
 			m.addToResourceList(i)
